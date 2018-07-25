87591f5fa34bed98e02c616ae240186b0b79966c
David Sutherland <david@wwwebbify.com>
2018-07-25 07:12:08 -0600

    Init

---
+++ quickSort.js
@@ -0,0 +1,18 @@
+function quickSort(array){
+
+if(array.length <= 1){return array;}
+
+var pivot = array[0];
+var lower = [];
+var equal = [];
+var higher = [];
+
+array.forEach(function(item){
+ if (item < pivot){
+  lower.push(item)
+ } else if (item === pivot){
+  equal.push(item)
+ } else {
+  higher.push(item)
+ }
+});
