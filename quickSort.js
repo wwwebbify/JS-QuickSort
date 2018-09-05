4b8cb3c053685e8c3afb57391e850d3be0cee2d3
David Sutherland <david@wwwebbify.com>
2018-09-05 07:45:46 -0600

    Added load balancing
    Improved performance by roughly 20%

--- quickSort.js
+++ quickSort.js
@@ -1,21 +1,53 @@
-function quickSort(array){
+function quickSort(array, max, min){
 
 if(array.length <= 1){return array;}
 
-var pivot = array[0];
+function findMax(arr){
+ var max = arr[0];
+ for(var i = 1; i<arr.length; i++){
+  if(arr[i] > max){
+   max = arr[i];
+  }
+ }
+ return max;
+}
+
+function findMin(arr){
+ var min = arr[0];
+ for(var i = 1; i<arr.length; i++){
+  if(arr[i] < min){
+   min = arr[i];
+  }
+ }
+ return min;
+}
+
+var max = typeof max === 'undefined' ? findMax(array) : max ;
+var min = typeof min === 'undefined' ? findMin(array) : min;
+var pivot = Math.floor((max - min)/2 + min);
 var lower = [];
+var lowerMax = 0;
+var lowerMin = min
 var equal = [];
 var higher = [];
+var higherMax = max;
+var higherMin = max;
 
 array.forEach(function(item){
  if (item < pivot){
+  if(item > lowerMax){
+   lowerMax = item;
+  }
   lower.push(item)
  } else if (item === pivot){
   equal.push(item)
  } else {
+if(item < higherMin){
+   higherMin = item;
+  }
   higher.push(item)
  }
 });
 
-return quickSort(lower).concat(equal, quickSort(higher));
+return quickSort(lower, lowerMax, lowerMin).concat(equal, quickSort(higher, higherMax, higherMin));
 }
